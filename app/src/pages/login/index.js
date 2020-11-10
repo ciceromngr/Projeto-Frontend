f08d0af70116de1a3da771bb04d2199d205821a8
cicero romao <cicerotransformice@gmail.com>
2020-11-10 10:36:02 -0300

    tela Login

--- app/src/pages/login/index.js
+++ app/src/pages/login/index.js
@@ -1,25 +1,33 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState , useEffect} from 'react';
 import { Body } from './styles';
 
 import { useAuth } from '../../hooks/auth';
 import { useHistory } from 'react-router-dom';
+import api from '../../services/api';
 const Login = () => {
     const history = useHistory();
     const { signIn } = useAuth();
   
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
+    const [usuario, setUsuario] = useState([]);
+
+    const user = useCallback(
+        async () => {
+            const response = await api.get('/usuario');
+            setUsuario(response.data);
+        },[])
+
+        useEffect(() => {
+            user();
+        }, [user]);
   
     const handleSubmit = async (e) => {
       e.preventDefault(); 
-      console.log(senha, email)
+
       try {
-          
-        await signIn(email, senha);
-        console.log("response ", email, senha);
-        console.log("handleSubmit success");
-  
-        history.push('/paginaPrincipal');
+        await signIn(email,senha);
+        history.push('/paginaPrincipal'); 
       } catch (error) {
         console.log("handleSubmit error", error);
       } 
--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -3,19 +3,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 
-import paginaDivEstilizada from '../pages/paginaComDivEstilizada';
+
 import PaginaPrincipal from '../pages/PaginaPrincipal';
-import ScrollLAteral from '../pages/ScrollLeft';
-import efeitoDiv from '../pages/estiloBotaoPersonalizado';
 import Login from '../pages/login';
 
 const Routes = () => (
     <Switch>
         <Route path="/" component={Login} exact />
         <Route path="/paginaPrincipal" component={PaginaPrincipal} />
-        <Route path="/paginaDivEstilizada" component={paginaDivEstilizada}/>
-        <Route path="/scrollLAteral" component={ScrollLAteral}/>
-        <Route path="/efeitoDiv" component={efeitoDiv}/>
     </Switch>
 )
 
